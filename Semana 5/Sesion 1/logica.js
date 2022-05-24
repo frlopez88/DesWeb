3d29bffd0d4fd5601a8cad606232ec4b03e7feb5
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-23 19:24:57 -0600

    Explicacion de getElementById

--- Semana 5/Sesion 1/logica.js
+++ Semana 5/Sesion 1/logica.js
@@ -7,12 +7,21 @@ function saludo(){
 
 function logIn(){
 
-
-	var correo = document.getElementById('email').value;	
+	var correo;
+	correo =  document.getElementById('email').value;	
 	var pass = document.getElementById('password').value;
-
 	var strresult = "El usuario es: " + correo + " y contrasenia: "+ pass;
-
 	document.getElementById('Pizarra').innerHTML=strresult;
 
 }
+
+// document = a la pagina donde esta instalado el script
+// getElementById () este metodo va a buscar el elemento
+// cuyo ID sera el parametro que reciba 
+// getElementById('pepito') 
+// ir a buscar en el documento html el elemento que 
+// se llame por id = 'pepito'
+
+// el elemento se debe operar 
+// pepito.value 
+// pepit.innetHTML = "Hola"
