92aa26e02b7c3e62800c0fc205e6179d28222bdb
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-30 19:35:52 -0600

    Semana 6 Sesion 1

---
+++ Semana 6/Sesion 1/logica.js
@@ -0,0 +1,121 @@
+
+var a = "Mundo";
+incrementales(5);
+
+
+
+
+function hola(){
+
+	let x = "Hola";
+	let sumatoria = x + a;
+	document.write(sumatoria);
+
+}
+
+function adios(){
+
+	let x = "Esta es una nueva variable";
+	var a = "Es la variable a nueva";
+
+	const j = "variable constante";
+
+	document.write("Este es el adios de la variable x "+x + "<br>");
+	document.write("Este es el adios de la variable a "+a+ "<br>");
+
+	j = "A un nuevo valor";
+
+	document.write("Este es el adios de la variable j "+j+"<br>")
+}
+
+
+function suma (){
+
+	let valor1 = "Hola";
+	let valor2 = 2.0;
+	let suma = valor1 + valor2;
+	document.write("Este es el valor de la suma "+suma+"<br>");
+	valor1 = 9.6;
+	suma = valor1 + valor2;
+	document.write("Este es el valor de la suma "+suma+"<br>");
+	var b = "Batman";
+
+	funcion_interna();
+
+
+	function funcion_interna() {
+		document.write("Este es el valor de la variable b "+b+"<br>");
+	}
+
+
+}
+
+
+
+function multiplicacion ( numero1 , numero2 ){
+
+	const mul = numero1*numero2;
+	document.write(mul);
+
+
+}
+
+
+
+function potencia (numero1, numero2){
+
+
+	const pot = numero1**numero2;
+	document.write(pot);
+
+}
+
+
+function division(numero1, numero2){
+
+
+	const div = numero1/numero2;
+	const mod = numero1%numero2;
+
+
+	document.write("Dividendo:"+numero1 + " Divisor:"+ numero2 +" Resultado:"+ div + " Residuo:"+mod);
+
+}
+
+
+function incrementales (numero) {
+
+
+	numero++;	
+	const prueba1 = numero;
+	numero--;
+	const prueba2 = numero;
+
+	// Otros operadores incrementales 
+
+	numero += 10;
+	numero -= 2;
+
+	document.write("Prueba 1:" + prueba1 + " Prueba2:"+ prueba2 + " numero:"+numero);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
